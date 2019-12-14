9779ecaffbd1c0262d54d8967962a934bfcc182e
Bobbie Omobolabo Teriba <bobbieteriba@gmail.com>
2019-12-14 22:32:40 +0000

    First Commit
    Added Unity Project to GitHub

---
+++ Wicklow Tour/Assets/Scripts/ImageVisible.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageVisible : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject image;
+
+    public void OpenImage()
+    {
+        if(image != null)
+        {
+            bool isActive = image.activeSelf;
+            image.SetActive(!isActive);
+            
+        }  
+    }
+
+    public void CloseImage()
+    {
+        if(image != null)
+        {
+            image.SetActive(false);
+        }
+    }
+
+}
