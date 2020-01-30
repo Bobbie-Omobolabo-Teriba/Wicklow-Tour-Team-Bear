3b23ac565e454b2db8bf112f69cb027c5bd8f1ac
Bobbie Omobolabo Teriba <bobbieteriba@gmail.com>
2020-01-30 14:34:34 +0000

    Mouse controls added
    Free Flight script added to enable mouse movement in desktop mode

---
+++ Wicklow Tour/Assets/Scripts/D0Reticle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class D0Reticle : MonoBehaviour
+{
+    public Image imgCircle;
+    public UnityEvent GVRClick;
+    public float totalTime = 2;
+    bool gvrstatus;
+    public float gvrTimer;
+}
---
+++ Wicklow Tour/Assets/Scripts/DragDropHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class DragDropHandler : MonoBehaviour
+{
+    public Image imgCircle;
+    public UnityEvent GVRClick;
+    public float totalTime = 5;
+    bool gvrstatus;
+    public float gvrTimer;
+}
