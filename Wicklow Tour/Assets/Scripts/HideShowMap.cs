77ff440d911ef583822059d8a255958c7483125f
Bobbie Omobolabo Teriba <bobbieteriba@gmail.com>
2020-02-25 01:03:31 +0000

    C# script to hide and show mini map
    This script should enable and disable the mini map when moving through scenes

---
+++ Wicklow Tour/Assets/Scripts/HideShowMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideShowMap1 : MonoBehaviour
+{
+    public GameObject miniMap1;
+
+    public GameObject Sphere1;
+    public GameObject Sphere2;
+    public GameObject Sphere3;
+    public GameObject Sphere4;
+
+    void Start()
+    {
+        miniMap1.SetActive(false);
+
+    }
+}
